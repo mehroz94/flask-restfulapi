e5c4b7363c1f77472c67900f27879a51e9104574
mehroz <mehroz.ahmed94@gmail.com>
2018-07-26 16:25:38 +0500

    Adding settings file. In this file, we will also declare the environment.

---
+++ settings.py
@@ -0,0 +1,7 @@
+# Settings/Config file
+APP_SETTINGS = 'development'
+
+SECRET_KEY = 'qw!wsadQQed1222s'
+DATABASE_URL = 'mysql://root:@localhost/flask-api'
+FLASK_APP = 'run.py'
+JWT_SECRET_KEY = 'uiqebb2hwb8!'
