cf7b2a780df2b8bda1bafe947d5c60b5e0517b95
mehroz <mehroz.ahmed94@gmail.com>
2018-07-30 17:38:58 +0500

    Adding shell command support.

--- manage.py
+++ manage.py
@@ -8,6 +8,13 @@ app = create_app(config_name=settings.APP_SETTINGS)
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+@manager.shell
+def shell():
+    return {
+        'app': app,
+        'db': db,
+        'models': None,
+    }
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
